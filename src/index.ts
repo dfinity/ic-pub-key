f7946576c7846b2d47bd7a03671f79b35843349b
Max Murphy <max@winning.black>
2025-05-22 13:49:38 +0200

    ++

---
+++ src/index.ts
@@ -0,0 +1,20 @@
+#!/usr/bin/env node
+
+import { Command } from "commander";
+
+const program = new Command();
+
+program
+  .name("mycli")
+  .description("A simple CLI app written in TypeScript")
+  .version("1.0.0");
+
+program
+  .command("greet <name>")
+  .description("Greet a user")
+  .action((name: string) => {
+    console.log(`Hello, ${name}!`);
+  });
+
+program.parse(process.argv);
+
