1551e12c938aeee4176bc9ea2abe0be2bd01cfba
Max Murphy <max@winning.black>
2025-05-22 17:36:11 +0200

    ++

--- src/ecdsa_public_key/secp256k1_public_key.rs
+++ src/ecdsa_public_key/secp256k1_public_key.rs
@@ -1,28 +1,23 @@
+use ic_secp256k1::{DerivationIndex, DerivationPath};
+use icrc_ledger_types::icrc1::account::Account;
+
+
+#[derive(Debug, PartialEq, Eq)]
+pub struct ECDSAPublicKey {
+    chain_code: Vec<u8>,
+    public_key: Vec<u8>,
+}
+
+
 #[cfg(test)]
 mod tests {
 
     use pretty_assertions::assert_eq;
-
-    #[derive(Debug, PartialEq, Eq)]
-    pub struct Response {
-        chain_code: Vec<u8>,
-        public_key: Vec<u8>,
-    }
-
-    pub fn derive_pub_key(
-        pub_key_without_derivation_path: Response,
-        derivation_path: Vec<Vec<u8>>,
-    ) -> Response {
-        todo!(
-            "derive_pub_key {:?} {:?}",
-            pub_key_without_derivation_path,
-            derivation_path
-        );
-    }
+    use super::*;
 
     #[test]
     fn derivation_works() {
-        let pub_key_without_derivation_path = Response {
+        let pub_key_without_derivation_path = ECDSAPublicKey {
             chain_code: vec![
                 33, 40, 145, 188, 3, 47, 40, 211, 105, 186, 207, 57, 220, 54, 159, 235, 81, 110,
                 206, 217, 163, 216, 52, 152, 36, 106, 234, 209, 84, 111, 140, 209,
@@ -37,7 +32,7 @@ mod tests {
             "4".repeat(3).as_bytes().to_vec(),
             "6".repeat(5).as_bytes().to_vec(),
         ];
-        let pub_key_with_derivation_path = Response {
+        let pub_key_with_derivation_path = ECDSAPublicKey {
             chain_code: vec![
                 188, 53, 184, 81, 95, 3, 170, 21, 67, 8, 30, 42, 244, 232, 120, 242, 139, 39, 243,
                 206, 0, 192, 53, 244, 6, 135, 2, 211, 62, 232, 133, 134,
@@ -48,8 +43,40 @@ mod tests {
             ],
         };
         assert_eq!(
-            derive_pub_key(pub_key_without_derivation_path, derivation_path),
+            derive_public_key(&pub_key_without_derivation_path, &derivation_path),
             pub_key_with_derivation_path
         );
+    }
+}
+
+pub fn derivation_path(simple: &Vec<Vec<u8>>) -> DerivationPath {
+    DerivationPath::new(
+        simple
+            .iter()
+            .map(|x| DerivationIndex(x.to_vec()))
+            .collect(),
+    )
+}
+
+pub fn derive_public_key(ecdsa_public_key: &ECDSAPublicKey, simple_derivation_path: &Vec<Vec<u8>>) -> ECDSAPublicKey {
+    use ic_secp256k1::{DerivationIndex, DerivationPath, PublicKey};
+
+    let path = derivation_path(simple_derivation_path);
+
+    let pk = PublicKey::deserialize_sec1(&ecdsa_public_key.public_key)
+        .expect("Failed to parse ECDSA public key");
+
+    let chain_code: [u8; 32] = ecdsa_public_key
+        .chain_code
+        .clone()
+        .try_into()
+        .expect("Incorrect chain code size");
+
+    let (derived_public_key, derived_chain_code) =
+        pk.derive_subkey_with_chain_code(&path, &chain_code);
+
+    ECDSAPublicKey {
+        public_key: derived_public_key.serialize_sec1(true),
+        chain_code: derived_chain_code.to_vec(),
     }
 }
