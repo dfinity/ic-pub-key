6026b21a53465646a198678d62d0360d5337f3f4
Max Murphy <max@winning.black>
2025-05-26 13:06:32 +0200

    ++

--- src/ecdsa_public_key/secp256k1_public_key.ts
+++ src/ecdsa_public_key/secp256k1_public_key.ts
@@ -42,15 +42,45 @@ export function test_derive_public_key() {
         ]),
     };
 
+    console.log("Public key without derivation path: ");
     console.log(pub_key_without_derivation_path);
 
 
     const derivation_path: DerivationPath = [ "2", "444", "66666"].map(s => new TextEncoder().encode(s));
 
-    //const pub_key_with_derivation_path = derive_public_key(pub_key_without_derivation_path, derivation_path);
+    console.log("Derivation path: ");
+    console.log(derivation_path);
 
+    const expected_pub_key_with_derivation_path = {
+        chain_code: new Uint8Array([
+            188, 53, 184, 81, 95, 3, 170, 21, 67, 8, 30, 42, 244, 232, 120, 242, 139, 39, 243,
+            206, 0, 192, 53, 244, 6, 135, 2, 211, 62, 232, 133, 134,
+        ]),
+        public_key: new Uint8Array([
+            2, 75, 247, 142, 64, 187, 81, 210, 198, 193, 76, 17, 170, 143, 58, 241, 84, 151,
+            65, 222, 90, 205, 249, 37, 230, 220, 35, 13, 252, 93, 170, 34, 217,
+        ]),
+    };
 
     const pub_key_with_derivation_path = derive_public_key(pub_key_without_derivation_path, derivation_path);
 
+    console.log("Derived public key: ");
     console.log(pub_key_with_derivation_path);
+
+    console.log("Expected public key: ");
+    console.log(expected_pub_key_with_derivation_path);
+
+    // Verify that the derived public key matches the expected public key
+    if (!deepEqual(pub_key_with_derivation_path, expected_pub_key_with_derivation_path)) {
+        throw new Error("Derived public key does not match expected public key");
+    }
 }
+
+
+function deepEqual(x: any, y: any): boolean {
+    const ok = Object.keys, tx = typeof x, ty = typeof y;
+    return x && y && tx === 'object' && tx === ty ? (
+      ok(x).length === ok(y).length &&
+        ok(x).every(key => deepEqual(x[key], y[key]))
+    ) : (x === y);
+  }
