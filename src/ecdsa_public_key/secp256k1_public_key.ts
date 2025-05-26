e10bdc072f030a070a1057396f6e9ae64f37b2d0
Max Murphy <max@winning.black>
2025-05-26 17:18:43 +0200

    ++

--- src/ecdsa_public_key/secp256k1_public_key.ts
+++ src/ecdsa_public_key/secp256k1_public_key.ts
@@ -1,23 +1,21 @@
-import { AffinePoint, ProjectivePoint } from "@noble/secp256k1";
+import { AffinePoint, ProjectivePoint } from '@noble/secp256k1';
 import { strict as assert } from 'assert';
 
 class PublicKeyWithChainCode {
-    constructor(
-        public readonly chain_code: ChainCode,
-        public readonly public_key: Sec1EncodedPublicKey
-    ) {}
+	constructor(
+		public readonly chain_code: ChainCode,
+		public readonly public_key: Sec1EncodedPublicKey
+	) {}
 
-    static fromArray(chain_code_array: number[], public_key_array: number[]): PublicKeyWithChainCode {
-        return new PublicKeyWithChainCode(
-            ChainCode.fromArray(chain_code_array),
-            Sec1EncodedPublicKey.fromArray(public_key_array)
-        );
-    }
+	static fromArray(chain_code_array: number[], public_key_array: number[]): PublicKeyWithChainCode {
+		return new PublicKeyWithChainCode(
+			ChainCode.fromArray(chain_code_array),
+			Sec1EncodedPublicKey.fromArray(public_key_array)
+		);
+	}
 
-    derive_subkey_with_chain_code(
-        derivation_path: DerivationPath,
-    ) : PublicKeyWithChainCode {
-        /*
+	derive_subkey_with_chain_code(derivation_path: DerivationPath): PublicKeyWithChainCode {
+		/*
         let public_key: AffinePoint = *self.key.as_affine();
         let (pt, _offset, chain_code) = derivation_path.derive_offset(public_key, chain_code);
     
@@ -29,71 +27,75 @@ class PublicKeyWithChainCode {
     
         (derived_key, chain_code)
         */
-        let public_key = this.public_key.asAffinePoint();
-        let todo = derivation_path.derive_offset(public_key, this.chain_code);
-        throw new Error("Not finished: derive_subkey_with_chain_code");
-    }    
+		let public_key = this.public_key.asAffinePoint();
+		let todo = derivation_path.derive_offset(public_key, this.chain_code);
+		throw new Error('Not finished: derive_subkey_with_chain_code');
+	}
 }
 /**
  * A public key, represented as a 33 byte array using sec1 encoding.
  */
 class Sec1EncodedPublicKey {
-    static readonly LENGTH = 33;
+	static readonly LENGTH = 33;
 
-    constructor(public readonly bytes: Uint8Array) {
-        if (bytes.length !== Sec1EncodedPublicKey.LENGTH) {
-            throw new Error(`Invalid PublicKey length: expected ${Sec1EncodedPublicKey.LENGTH} bytes, got ${bytes.length}`);
-        }
-    }
+	constructor(public readonly bytes: Uint8Array) {
+		if (bytes.length !== Sec1EncodedPublicKey.LENGTH) {
+			throw new Error(
+				`Invalid PublicKey length: expected ${Sec1EncodedPublicKey.LENGTH} bytes, got ${bytes.length}`
+			);
+		}
+	}
 
-    static fromArray(array: number[]): Sec1EncodedPublicKey {
-        return new Sec1EncodedPublicKey(new Uint8Array(array));
-    }
+	static fromArray(array: number[]): Sec1EncodedPublicKey {
+		return new Sec1EncodedPublicKey(new Uint8Array(array));
+	}
 
-    static fromProjectivePoint(point: ProjectivePoint): Sec1EncodedPublicKey {
-        const hex = point.toHex();
-        const bytes = Buffer.from(hex, 'hex');
-        return new Sec1EncodedPublicKey(new Uint8Array(bytes));
-    }
+	static fromProjectivePoint(point: ProjectivePoint): Sec1EncodedPublicKey {
+		const hex = point.toHex();
+		const bytes = Buffer.from(hex, 'hex');
+		return new Sec1EncodedPublicKey(new Uint8Array(bytes));
+	}
 
-    asHex(): string {
-        return Buffer.from(this.bytes).toString('hex');
-    }
+	asHex(): string {
+		return Buffer.from(this.bytes).toString('hex');
+	}
 
-    asProjectivePoint(): ProjectivePoint {
-        return ProjectivePoint.fromHex(this.asHex());
-    }
+	asProjectivePoint(): ProjectivePoint {
+		return ProjectivePoint.fromHex(this.asHex());
+	}
 
-    asAffinePoint(): AffinePoint {
-        return this.asProjectivePoint().toAffine();
-    }
+	asAffinePoint(): AffinePoint {
+		return this.asProjectivePoint().toAffine();
+	}
 
-    asAffineHex(): string {
-        let affine_point = this.asAffinePoint();
-        const x = affine_point.x;
-        const y = affine_point.y;
-        return "x: " + x.toString(16) + " y: " + y.toString(16);
-    }
-    x_as_hex(): string {
-        const x = this.asAffinePoint().x;
-        return x.toString(16);
-    }
+	asAffineHex(): string {
+		let affine_point = this.asAffinePoint();
+		const x = affine_point.x;
+		const y = affine_point.y;
+		return 'x: ' + x.toString(16) + ' y: ' + y.toString(16);
+	}
+	x_as_hex(): string {
+		const x = this.asAffinePoint().x;
+		return x.toString(16);
+	}
 }
 /**
  * A chain code is a 32 byte array
  */
 class ChainCode {
-    static readonly LENGTH = 32;
-  
-    constructor(public readonly bytes: Uint8Array) {
-      if (bytes.length !== ChainCode.LENGTH) {
-        throw new Error(`Invalid ChainCode length: expected ${ChainCode.LENGTH} bytes, got ${bytes.length}`);
-      }
-    }
+	static readonly LENGTH = 32;
 
-    static fromArray(array: number[]): ChainCode {
-        return new ChainCode(new Uint8Array(array));
-    }
+	constructor(public readonly bytes: Uint8Array) {
+		if (bytes.length !== ChainCode.LENGTH) {
+			throw new Error(
+				`Invalid ChainCode length: expected ${ChainCode.LENGTH} bytes, got ${bytes.length}`
+			);
+		}
+	}
+
+	static fromArray(array: number[]): ChainCode {
+		return new ChainCode(new Uint8Array(array));
+	}
 }
 
 /**
@@ -102,10 +104,10 @@ class ChainCode {
 type PathComponent = Uint8Array;
 
 class DerivationPath {
-    constructor(public readonly path: PathComponent[]) {}
+	constructor(public readonly path: PathComponent[]) {}
 
-    derive_offset(pt: AffinePoint, chain_code: ChainCode): [AffinePoint, bigint, ChainCode] {
-    /*
+	derive_offset(pt: AffinePoint, chain_code: ChainCode): [AffinePoint, bigint, ChainCode] {
+		/*
         fn derive_offset(
         &self,
         pt: AffinePoint,
@@ -125,16 +127,20 @@ class DerivationPath {
         (pt, offset, chain_code)
     }
     */
-   let offset = BigInt(0);
+		let offset = BigInt(0);
 
-   for (let idx of this.path) {
-    let [next_chain_code, next_offset, next_pt] = DerivationPath.ckd_pub(idx, pt, chain_code);
-   }
-    throw new Error("Not implemented: DerivationPath.derive_offset");
-    }
+		for (let idx of this.path) {
+			let [next_chain_code, next_offset, next_pt] = DerivationPath.ckd_pub(idx, pt, chain_code);
+		}
+		throw new Error('Not implemented: DerivationPath.derive_offset');
+	}
 
-    static ckd_pub(idx: PathComponent, pt: AffinePoint, chain_code: ChainCode): [ChainCode, bigint, AffinePoint] {
-        /*
+	static ckd_pub(
+		idx: PathComponent,
+		pt: AffinePoint,
+		chain_code: ChainCode
+	): [ChainCode, bigint, AffinePoint] {
+		/*
             fn ckd_pub(
             idx: &[u8],
             pt: AffinePoint,
@@ -166,22 +172,24 @@ class DerivationPath {
         }
     
     */
-        let ckd_input = ProjectivePoint.fromAffine(pt).toRawBytes(true);
-        let projective_point = ProjectivePoint.fromAffine(pt);
+		let ckd_input = ProjectivePoint.fromAffine(pt).toRawBytes(true);
+		let projective_point = ProjectivePoint.fromAffine(pt);
 
-        while (true) {
-            let [next_chain_code, next_offset] = DerivationPath.ckd(idx, ckd_input, chain_code);
+		while (true) {
+			let [next_chain_code, next_offset] = DerivationPath.ckd(idx, ckd_input, chain_code);
 
-            //let next_pt = (pt + k256::ProjectivePoint::mul_by_generator(&next_offset)).to_affine();
+			//let next_pt = (pt + k256::ProjectivePoint::mul_by_generator(&next_offset)).to_affine();
+		}
 
-        }
+		throw new Error('Not implemented: DerivationPath.ckd_pub');
+	}
 
-
-        throw new Error("Not implemented: DerivationPath.ckd_pub");
-    }
-
-    static ckd(idx: PathComponent, ckd_input: Uint8Array, chain_code: ChainCode): [ChainCode, bigint] {
-        /*
+	static ckd(
+		idx: PathComponent,
+		ckd_input: Uint8Array,
+		chain_code: ChainCode
+	): [ChainCode, bigint] {
+		/*
             fn ckd(idx: &[u8], input: &[u8], chain_code: &[u8; 32]) -> ([u8; 32], Scalar) {
         use hmac::{Hmac, Mac};
         use k256::{elliptic_curve::ops::Reduce, sha2::Sha512};
@@ -209,17 +217,16 @@ class DerivationPath {
         }
     }
         */
-        throw new Error("Not implemented: DerivationPath.ckd");
-    }
+		throw new Error('Not implemented: DerivationPath.ckd');
+	}
 }
-
 
 // Translated from Rust: mkpubkey::ecdsa_public_key::secp256k1_public_key
 export function derive_public_key(
-    ecdsa_public_key: PublicKeyWithChainCode,
-    simple_derivation_path: DerivationPath,
-) : PublicKeyWithChainCode {
-/*
+	ecdsa_public_key: PublicKeyWithChainCode,
+	simple_derivation_path: DerivationPath
+): PublicKeyWithChainCode {
+	/*
 pub fn derive_public_key(
     ecdsa_public_key: &ECDSAPublicKey,
     simple_derivation_path: &Vec<Vec<u8>>,
@@ -248,78 +255,91 @@ pub fn derive_public_key(
 
 */
 
-    console.log("Affine hex: ", ecdsa_public_key.public_key.asAffineHex());
+	console.log('Affine hex: ', ecdsa_public_key.public_key.asAffineHex());
 
-    let derived_key = ecdsa_public_key.derive_subkey_with_chain_code(simple_derivation_path);
+	let derived_key = ecdsa_public_key.derive_subkey_with_chain_code(simple_derivation_path);
 
-    return derived_key;
+	return derived_key;
 }
-
 
 // TODO: Test error thrown on invalid chain code
 // TODO: Test error thrown on invalid derivation path
 // TODO: Test error thrown on invalid public key
 
-
 // Tests the public key derivation
 export function test_derive_public_key() {
-    const pub_key_without_derivation_path = PublicKeyWithChainCode.fromArray(
-        [
-            33, 40, 145, 188, 3, 47, 40, 211, 105, 186, 207, 57, 220, 54, 159, 235, 81, 110,
-            206, 217, 163, 216, 52, 152, 36, 106, 234, 209, 84, 111, 140, 209,
-        ],
-        [
-            2, 184, 79, 243, 248, 131, 41, 168, 135, 101, 125, 3, 9, 189, 26, 26, 249, 227,
-            118, 1, 229, 209, 165, 53, 214, 254, 125, 66, 227, 127, 121, 244, 10,
-        ]
-    );
+	const pub_key_without_derivation_path = PublicKeyWithChainCode.fromArray(
+		[
+			33, 40, 145, 188, 3, 47, 40, 211, 105, 186, 207, 57, 220, 54, 159, 235, 81, 110, 206, 217,
+			163, 216, 52, 152, 36, 106, 234, 209, 84, 111, 140, 209
+		],
+		[
+			2, 184, 79, 243, 248, 131, 41, 168, 135, 101, 125, 3, 9, 189, 26, 26, 249, 227, 118, 1, 229,
+			209, 165, 53, 214, 254, 125, 66, 227, 127, 121, 244, 10
+		]
+	);
 
-    {
-      console.log("Public key without derivation path: ");
-      console.log(pub_key_without_derivation_path);
-      assert.equal(pub_key_without_derivation_path.public_key.x_as_hex(), "b84ff3f88329a887657d0309bd1a1af9e37601e5d1a535d6fe7d42e37f79f40a");
-      // Check that conversion to/from projective point is identity
-      const projective_point = pub_key_without_derivation_path.public_key.asProjectivePoint();
-      const converted_back = Sec1EncodedPublicKey.fromProjectivePoint(projective_point);
-      assert.deepEqual(converted_back, pub_key_without_derivation_path.public_key, "Conversion to/from projective point is not identity");
-    }
+	{
+		console.log('Public key without derivation path: ');
+		console.log(pub_key_without_derivation_path);
+		assert.equal(
+			pub_key_without_derivation_path.public_key.x_as_hex(),
+			'b84ff3f88329a887657d0309bd1a1af9e37601e5d1a535d6fe7d42e37f79f40a'
+		);
+		// Check that conversion to/from projective point is identity
+		const projective_point = pub_key_without_derivation_path.public_key.asProjectivePoint();
+		const converted_back = Sec1EncodedPublicKey.fromProjectivePoint(projective_point);
+		assert.deepEqual(
+			converted_back,
+			pub_key_without_derivation_path.public_key,
+			'Conversion to/from projective point is not identity'
+		);
+	}
 
+	const derivation_path = new DerivationPath(
+		['2', '444', '66666'].map((s) => new TextEncoder().encode(s))
+	);
 
-    const derivation_path = new DerivationPath(["2", "444", "66666"].map(s => new TextEncoder().encode(s)));
+	console.log('Derivation path: ');
+	console.log(derivation_path);
 
-    console.log("Derivation path: ");
-    console.log(derivation_path);
+	const expected_pub_key_with_derivation_path = new PublicKeyWithChainCode(
+		new ChainCode(
+			new Uint8Array([
+				188, 53, 184, 81, 95, 3, 170, 21, 67, 8, 30, 42, 244, 232, 120, 242, 139, 39, 243, 206, 0,
+				192, 53, 244, 6, 135, 2, 211, 62, 232, 133, 134
+			])
+		),
+		new Sec1EncodedPublicKey(
+			new Uint8Array([
+				2, 75, 247, 142, 64, 187, 81, 210, 198, 193, 76, 17, 170, 143, 58, 241, 84, 151, 65, 222,
+				90, 205, 249, 37, 230, 220, 35, 13, 252, 93, 170, 34, 217
+			])
+		)
+	);
 
-    const expected_pub_key_with_derivation_path = new PublicKeyWithChainCode(
-        new ChainCode(new Uint8Array([
-            188, 53, 184, 81, 95, 3, 170, 21, 67, 8, 30, 42, 244, 232, 120, 242, 139, 39, 243,
-            206, 0, 192, 53, 244, 6, 135, 2, 211, 62, 232, 133, 134,
-        ])),
-        new Sec1EncodedPublicKey(new Uint8Array([
-            2, 75, 247, 142, 64, 187, 81, 210, 198, 193, 76, 17, 170, 143, 58, 241, 84, 151,
-            65, 222, 90, 205, 249, 37, 230, 220, 35, 13, 252, 93, 170, 34, 217,
-        ]))
-    );
+	const pub_key_with_derivation_path = derive_public_key(
+		pub_key_without_derivation_path,
+		derivation_path
+	);
 
-    const pub_key_with_derivation_path = derive_public_key(pub_key_without_derivation_path, derivation_path);
+	console.log('Derived public key: ');
+	console.log(pub_key_with_derivation_path);
 
-    console.log("Derived public key: ");
-    console.log(pub_key_with_derivation_path);
+	console.log('Expected public key: ');
+	console.log(expected_pub_key_with_derivation_path);
 
-    console.log("Expected public key: ");
-    console.log(expected_pub_key_with_derivation_path);
-
-    // Verify that the derived public key matches the expected public key
-    if (!deepEqual(pub_key_with_derivation_path, expected_pub_key_with_derivation_path)) {
-        throw new Error("Derived public key does not match expected public key");
-    }
+	// Verify that the derived public key matches the expected public key
+	if (!deepEqual(pub_key_with_derivation_path, expected_pub_key_with_derivation_path)) {
+		throw new Error('Derived public key does not match expected public key');
+	}
 }
 
-
 function deepEqual(x: any, y: any): boolean {
-    const ok = Object.keys, tx = typeof x, ty = typeof y;
-    return x && y && tx === 'object' && tx === ty ? (
-      ok(x).length === ok(y).length &&
-        ok(x).every(key => deepEqual(x[key], y[key]))
-    ) : (x === y);
-  }
+	const ok = Object.keys,
+		tx = typeof x,
+		ty = typeof y;
+	return x && y && tx === 'object' && tx === ty
+		? ok(x).length === ok(y).length && ok(x).every((key) => deepEqual(x[key], y[key]))
+		: x === y;
+}
--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,20 @@
 #!/usr/bin/env node
 
-import { Command } from "commander";
-import { test_derive_public_key as test_derive_secp256k1_public_key } from "./ecdsa_public_key/secp256k1_public_key";
+import { Command } from 'commander';
+import { test_derive_public_key as test_derive_secp256k1_public_key } from './ecdsa_public_key/secp256k1_public_key';
 const program = new Command();
 
-program
-  .name("mycli")
-  .description("A simple CLI app written in TypeScript")
-  .version("1.0.0");
+program.name('mycli').description('A simple CLI app written in TypeScript').version('1.0.0');
+
+program.command('greet <name>').description('Greet a user').action(say_hello);
 
 program
-  .command("greet <name>")
-  .description("Greet a user")
-  .action(say_hello);
-
-program
-  .command("test-secp256k1")
-  .description("Derive a key")
-  .action(test_derive_secp256k1_public_key);
+	.command('test-secp256k1')
+	.description('Derive a key')
+	.action(test_derive_secp256k1_public_key);
 
 program.parse(process.argv);
 
 function say_hello(name: string) {
-    console.log(`Hello, ${name}!`);
+	console.log(`Hello, ${name}!`);
 }
