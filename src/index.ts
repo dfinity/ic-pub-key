2452a3eb4c9e4b7f59949cc0995ceded494ebd35
Max Murphy <max@winning.black>
2025-05-28 14:18:49 +0200

    ++

--- src/index.ts
+++ src/index.ts
@@ -7,6 +7,9 @@ import {
 	PublicKeyWithChainCode as Secp256k1PublicKeyWithChainCode,
 	test_derive_public_key as test_derive_secp256k1_public_key
 } from './ecdsa_public_key/secp256k1_public_key';
+import { Principal } from "@dfinity/principal";
+
+
 const program = new Command();
 
 program.name('mycli').description('A simple CLI app written in TypeScript').version('1.0.0');
@@ -37,8 +40,37 @@ program
 		console.log(JSON.stringify(ans, null, 2));
 	});
 
+program
+	.command('caller-secp256k1 <principal> <pubkey> <chaincode> <derivationpath>')
+	.description('Derive a key for the given principal')
+	.action((principal, pubkey, chaincode, derivationpath) => {
+		let parsed_principal = Principal.fromText(principal);
+		let derivation_path = principal_derivation_path(parsed_principal);
+		let pubkey_with_chain_code = Secp256k1PublicKeyWithChainCode.fromBlob(pubkey, chaincode);
+		let parsed_derivationpath = DerivationPath.fromBlob(derivationpath);
+		let combined_derivation_path = new DerivationPath([...derivation_path.path, ...parsed_derivationpath.path]);
+		let derived_pubkey = derive_secp256k1_public_key(pubkey_with_chain_code, parsed_derivationpath);
+		let ans = {
+			request: {
+				key: pubkey_with_chain_code,
+				derivation_path: combined_derivation_path
+			},
+			response: {
+				key: derived_pubkey
+			}
+		};
+		console.log(JSON.stringify(ans, null, 2));
+	});
+
+
 program.parse(process.argv);
 
 function say_hello(name: string) {
 	console.log(`Hello, ${name}!`);
+}
+
+function principal_derivation_path(principal: Principal): DerivationPath {
+	let schema = Uint8Array.from([0xff]);
+	let principal_bytes = principal.toUint8Array();
+	return new DerivationPath([schema, principal_bytes]);
 }
