637edd8d33b9b656db8b2469617140a1ae8e7c39
Max Murphy <max@winning.black>
2025-06-02 15:13:18 +0200

    ++

---
+++ src/index.ts
@@ -0,0 +1,4 @@
+#!/usr/bin/env node
+
+export * as ecdsa from './ecdsa/index';
+export * as schnorr from './schnorr/index';
---
+++ src/schnorr/index.ts
@@ -0,0 +1,4 @@
+#!/usr/bin/env node
+
+export * as bip340secp256k1 from './bip340secp256k1';
+export * as ed25519 from './ed25519';
