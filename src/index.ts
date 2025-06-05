243557128416bad97cf1e2b2f78260c5fc165806
Max Murphy <max@winning.black>
2025-06-05 05:56:45 +0200

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,2 @@
-#!/usr/bin/env node
-
-import { Command } from "commander";
-
-const program = new Command();
-
-program
-  .name("mycli")
-  .description("A simple CLI app written in TypeScript")
-  .version("1.0.0");
-
-program.command("greet <name>").description("Greet a user").action(say_hello);
-
-program.parse(process.argv);
-
-function say_hello(name: string) {
-  console.log(`Hello, ${name}!`);
-}
-
-function derive_key() {}
+// TODO: export * as ecdsa from './ecdsa/index';
+// TODO: export * as schnorr from './schnorr/index';
