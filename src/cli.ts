5f8e5c002b539ea4c3df95b47d5747567fcd219a
Max Murphy <max@winning.black>
2025-06-05 06:19:33 +0200

    ++

--- src/cli.ts
+++ src/cli.ts
@@ -1,11 +1,8 @@
-#!/usr/bin/env node
-
 import { Command } from "commander";
 
-const program = new Command();
+export const program = new Command();
 
 program
   .name("ic-pub-key")
   .description("Tools for Internet Computer Protocol public keys")
   .version("1.0.0");
-program.parse(process.argv);
