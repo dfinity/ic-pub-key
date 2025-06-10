6f3645ffbe946d79571aababf166d9a97d57925f
Max Murphy <max@winning.black>
2025-06-10 20:59:39 +0200

    ++

--- src/ecdsa/secp256k1.tests/intermedeiate_values.test.ts
+++ src/ecdsa/secp256k1.tests/intermedeiate_values.test.ts
@@ -9,6 +9,9 @@ interface CkdTestVector {
 	expected_scalar: BigInt;
 }
 
+// Source: These are arguments and corresponding outputs from calling the ckd function in Rust.
+// 
+// See: [ic_secp256k1::DerivationPath::ckd](https://github.com/dfinity/ic/blob/bb6e758c739768ef6713f9f3be2df47884544900/packages/ic-secp256k1/src/lib.rs#L111)
 const testVectors: CkdTestVector[] = [
 	{
 		idx: new Uint8Array([0x32]),
