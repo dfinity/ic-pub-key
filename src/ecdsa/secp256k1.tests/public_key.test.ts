952d1a7f2014eb7658b79f394a4280eebf7e83e8
Max Murphy <max@winning.black>
2025-06-12 01:09:29 +0200

    ++

--- src/ecdsa/secp256k1.tests/public_key.test.ts
+++ src/ecdsa/secp256k1.tests/public_key.test.ts
@@ -1,5 +1,5 @@
 import { describe, expect, it } from 'vitest';
-import { Sec1EncodedPublicKey } from '../secp256k1';
+import { ChainCode, Sec1EncodedPublicKey } from '../secp256k1';
 
 describe('Sec1EncodedPublicKey', () => {
 	it('should create a new Sec1EncodedPublicKey', () => {
@@ -35,5 +35,29 @@ describe('Sec1EncodedPublicKey', () => {
 		const hex_key = '000102030405060708090a0b0c0d0e0f101112131415161718191a1b1c1d1e1f20';
 		const publicKey = Sec1EncodedPublicKey.fromHex(hex_key);
 		expect(publicKey.toHex()).toBe(hex_key);
+	});
+});
+
+describe('ChainCode', () => {
+	it('should create a new ChainCode from 32 bytes', () => {
+		const chainCode = new ChainCode(
+			Buffer.from('000102030405060708090a0b0c0d0e0f101112131415161718191a1b1c1d1e1f', 'hex')
+		);
+	});
+	it('should fail to create a chain code from less than 32 bytes', () => {
+		expect(
+			() =>
+				new ChainCode(
+					Buffer.from('000102030405060708090a0b0c0d0e0f101112131415161718191a1b1c1d1e', 'hex')
+				)
+		).toThrow();
+	});
+	it('should fail to create a chain code from more than 32 bytes', () => {
+		expect(
+			() =>
+				new ChainCode(
+					Buffer.from('000102030405060708090a0b0c0d0e0f101112131415161718191a1b1c1d1e1f20', 'hex')
+				)
+		).toThrow();
 	});
 });
