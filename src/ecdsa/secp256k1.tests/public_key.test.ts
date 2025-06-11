3fe1790e97d51095a3d90a5d5292ecf177ed49a5
Max Murphy <max@winning.black>
2025-06-12 01:05:40 +0200

    ++

---
+++ src/ecdsa/secp256k1.tests/public_key.test.ts
@@ -0,0 +1,39 @@
+import { describe, expect, it } from 'vitest';
+import { Sec1EncodedPublicKey } from '../secp256k1';
+
+describe('Sec1EncodedPublicKey', () => {
+	it('should create a new Sec1EncodedPublicKey', () => {
+		const _publicKey = new Sec1EncodedPublicKey(
+			Buffer.from('000102030405060708090a0b0c0d0e0f101112131415161718191a1b1c1d1e1f20', 'hex')
+		);
+	});
+	it('should throw an exception if the key is less than 33 bytes', () => {
+		expect(
+			() =>
+				new Sec1EncodedPublicKey(
+					Buffer.from('000102030405060708090a0b0c0d0e0f101112131415161718191a1b1c1d1e1f', 'hex')
+				)
+		).toThrow();
+	});
+	it('should throw an exception if the key is more than 33 bytes', () => {
+		expect(
+			() =>
+				new Sec1EncodedPublicKey(
+					Buffer.from('000102030405060708090a0b0c0d0e0f101112131415161718191a1b1c1d1e1f2021', 'hex')
+				)
+		).toThrow();
+	});
+	it('should hex encode', () => {
+		const publicKey = new Sec1EncodedPublicKey(
+			Buffer.from('000102030405060708090a0b0c0d0e0f101112131415161718191a1b1c1d1e1f20', 'hex')
+		);
+		expect(publicKey.toHex()).toBe(
+			'000102030405060708090a0b0c0d0e0f101112131415161718191a1b1c1d1e1f20'
+		);
+	});
+	it('should hex decode and encode back to the same key', () => {
+		const hex_key = '000102030405060708090a0b0c0d0e0f101112131415161718191a1b1c1d1e1f20';
+		const publicKey = Sec1EncodedPublicKey.fromHex(hex_key);
+		expect(publicKey.toHex()).toBe(hex_key);
+	});
+});
