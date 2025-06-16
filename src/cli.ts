735d04ee58ea593ed14b8f7def8f0c58ed1acdea
Max Murphy <max@winning.black>
2025-06-16 18:10:49 +0200

    ++

--- src/cli.ts
+++ src/cli.ts
@@ -104,10 +104,11 @@ eth
 			Uint8Array.from([0x01]),
 			principal_as_bytes
 		]);
-		console.log("derivation_path", derivation_path.toHex());
 		let signer_pubkey_with_chain_code = Secp256k1PublicKeyWithChainCode.fromString(pubkey, chaincode);
         let eth_pubkey_with_chaincode = signer_pubkey_with_chain_code.derive_subkey_with_chain_code(derivation_path);
 		let eth_pubkey = eth_pubkey_with_chaincode.public_key;
+        console.log("root key:", signer_pubkey_with_chain_code.public_key.asHex());
+		console.log("derivation_path", derivation_path.toHex());
 		console.log("eth_pubkey", eth_pubkey.asHex());
 		let eth_address = computeAddress("0x" + eth_pubkey.asHex());
 		console.log(eth_address);
