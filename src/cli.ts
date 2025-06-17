7bcf92ccec6283d3901fb62cc8e54037cded6323
Max Murphy <max@winning.black>
2025-06-17 12:01:04 +0200

    ++

--- src/cli.ts
+++ src/cli.ts
@@ -1,4 +1,6 @@
+import { Principal } from '@dfinity/principal';
 import { Command } from 'commander';
+import { computeAddress } from 'ethers';
 import {
 	DerivationPath,
 	PublicKeyWithChainCode as Secp256k1PublicKeyWithChainCode
@@ -41,3 +43,37 @@ function ecdsa_secp256k1_derive(pubkey: string, chaincode: string, derivationpat
 	};
 	return JSON.stringify(ans, null, 2);
 }
+
+let signer = program.command('signer').description('Get chain fusion signer token address');
+
+let eth = signer.command('eth').description('Get Ethereum address');
+
+eth
+	.command('address')
+	.description("Get a user's Ethereum address")
+	.option('-p, --pubkey <pubkey>', "The signer canister's public key", String)
+	.option('-c, --chaincode <chaincode>', "The signer canister's chain code", String)
+	.requiredOption('-u, --user <user>', "The user's principal", String)
+	.action(({ pubkey, chaincode, user }) => {
+		pubkey =
+			pubkey == null
+				? '0259761672ec7ee3bdc5eca95ba5f6a493d1133b86a76163b68af30c06fe3b75c0'
+				: pubkey;
+		chaincode =
+			chaincode == null
+				? 'f666a98c7f70fe281ca8142f14eb4d1e0934a439237da83869e2cfd924b270c0'
+				: chaincode;
+		let principal = Principal.fromText(user);
+		let principal_as_bytes = principal.toUint8Array();
+		let derivation_path = new DerivationPath([Uint8Array.from([0x01]), principal_as_bytes]);
+		let signer_pubkey_with_chain_code = Secp256k1PublicKeyWithChainCode.fromString({
+			public_key: pubkey,
+			chain_code: chaincode
+		});
+		let eth_pubkey_with_chaincode =
+			signer_pubkey_with_chain_code.deriveSubkeyWithChainCode(derivation_path);
+		let eth_pubkey = eth_pubkey_with_chaincode.public_key;
+
+		let eth_address = computeAddress('0x' + eth_pubkey.toHex());
+		console.log('eth_address', eth_address);
+	});
