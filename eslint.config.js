81c36afe18f7ce066da22ca2569df8180d7800a3
Antonio Ventilii <antonioventilii@gmail.com>
2025-06-26 15:32:58 +0200

    lint: Add exceptions to rule `no-unused-vars` (#44)
    # Motivation
    
    Rule `@typescript-eslint/no-unused-vars requires` a few more exceptions
    for unused variables that we define as such.

--- eslint.config.js
+++ eslint.config.js
@@ -46,7 +46,14 @@ export default [
 		rules: {
 			...typescript.configs.recommended.rules,
 			...typescript.configs['recommended-requiring-type-checking'].rules,
-			'@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+			'@typescript-eslint/no-unused-vars': [
+				'error',
+				{
+					argsIgnorePattern: '^_',
+					varsIgnorePattern: '^_',
+					caughtErrorsIgnorePattern: '^_'
+				}
+			],
 			'@typescript-eslint/explicit-function-return-type': 'off',
 			'@typescript-eslint/explicit-module-boundary-types': 'off',
 			'@typescript-eslint/no-explicit-any': 'warn',
