dab8eb7e5cdf0b7b874b55eb59250f989de0c657
Max Murphy <max@winning.black>
2025-07-02 02:37:53 +0200

    Test that the point at infinity is not considered a valid public key

--- src/schnorr/ed25519.ts
+++ src/schnorr/ed25519.ts
@@ -1,9 +1,42 @@
+import { ExtendedPoint } from '@noble/ed25519';
 import { blobDecode, blobEncode } from '../encoding.js';
 
 /**
  * One part of a derivation path.
  */
 export type PathComponent = Uint8Array;
+
+export class PublicKey {
+	/**
+	 * The length of a public key in bytes.  As hex it is twice this.
+	 */
+	static readonly LENGTH = 32;
+
+	constructor(public readonly key: ExtendedPoint) {
+		if (key.is0()) {
+			throw new Error('Invalid public key');
+		}
+	}
+
+	/**
+	 * Creates a new PublicKey from a hex string.
+	 * @param hex The hex string to create the public key from.
+	 * @throws If the hex string has the wrong length for a public key.
+	 * @throws If the public key is the point at infinity.
+	 * @returns A new PublicKey.
+	 */
+	static fromHex(hex: string): PublicKey {
+		return new PublicKey(ExtendedPoint.fromHex(hex));
+	}
+
+	/**
+	 * Returns the public key as a hex string.
+	 * @returns A 64 character hex string.
+	 */
+	toHex(): string {
+		return this.key.toHex();
+	}
+}
 
 export class DerivationPath {
 	constructor(public readonly path: PathComponent[]) {}
