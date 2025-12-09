085297ab97f9be545889165664772133f1926ee1
Antonio Ventilii <antonio.ventilii@dfinity.org>
2025-12-09 11:02:07 +0100

    chore: Adjust vitest configs

--- vitest.config.ts
+++ vitest.config.ts
@@ -14,10 +14,12 @@ export default defineConfig({
 				'**/types.ts'
 			],
 			all: true,
-			branches: 80,
-			functions: 80,
-			lines: 80,
-			statements: 80
+			thresholds: {
+				branches: 80,
+				functions: 80,
+				lines: 80,
+				statements: 80
+			}
 		}
 	}
 });
