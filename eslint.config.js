16254f6bb72620aa8c1d058882174c2971412551
Antonio Ventilii <antonio.ventilii@dfinity.org>
2026-01-26 11:36:10 +0100

    lint

--- eslint.config.js
+++ eslint.config.js
@@ -35,7 +35,7 @@ export default [
 			parserOptions: {
 				ecmaVersion: 'latest',
 				sourceType: 'module',
-				project: './tsconfig.json'
+				project: './tsconfig.eslint.json'
 			},
 			globals: {
 				console: 'readonly',
